eef8acec14120fa6c1f6c6a9c07e74921358e7af
Raul Rodriguez <raulrobi@gmail.com>
2021-04-26 21:49:06 -0300

    Class 323 - Express server setup

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("hola mundo!!!");
+const express = require("express");
+
+//Express server
+const app = express();
+
+//Routes
+app.get("/", (req, res) => {
+    res.json({ok: true});
+});
+
+//Listening request
+app.listen(3001, () => {
+    console.log(`Server running on port ${4000}`);
+});
