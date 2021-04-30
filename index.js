c78ca78a89a9607b4789e6246a8667fde868c1e5
Raul Rodriguez <raulrobi@gmail.com>
2021-04-30 10:08:32 -0300

    Class 338 - CORS

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const {dbConnection} = require("./database/config");
+const cors = require("cors");
 require("dotenv").config();
 
 //Express server
@@ -7,6 +8,9 @@ const app = express();
 
 //DB Connection
 dbConnection();
+
+//CORS
+app.use(cors());
 
 //Public directory
 //El use es un middleware de express
